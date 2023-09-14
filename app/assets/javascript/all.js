db6bf171c3b80a1d4506392903e13d02238ca07d
theo.selenskiy <theo.selenskiy@engineering.digital.dwp.gov.uk>
2023-09-14 13:21:07 +0100

    feat(conditional rendering ): store user preference in a cookie and use to add or remove cookies from the page

--- app/assets/javascript/all.js
+++ app/assets/javascript/all.js
@@ -1,13 +1,40 @@
-//const { attachAcceptClickHandler, attachRejectClickHandler } = require('./cookie-message');
+function setCookie(name,value,days) {
+  console.log('inside setCookie');
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  console.log('set cookie: ', name);
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
 
 const attachAcceptClickHandler = (document) => {
   const acceptButton = document.querySelector('[value="accept"][name="cookies"]');
-  acceptButton.onclick = () => { console.log('CLICKED ACCEPT') };
+  acceptButton.onclick = () => { 
+    console.log('CLICKED ACCEPT');
+    setCookie('cookieChoiceMade', 'accept', 1);
+  };
 }
 
 const attachRejectClickHandler = (document) => {
   const rejectButton = document.querySelector('[value="reject"][name="cookies"]');
-  rejectButton.onclick = () => { console.log('CLICKED REJECT') };
+  rejectButton.onclick = () => { 
+    console.log('CLICKED REJECT');
+    setCookie('cookieChoiceMade', 'reject', 1);
+  };
 }
 
 function initAll() {
