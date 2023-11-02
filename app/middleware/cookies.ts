2b4c23382eee245d3dd8fef75a618e3b0aa987c1
theo.selenskiy <theo.selenskiy@engineering.digital.dwp.gov.uk>
2023-11-02 13:49:56 +0000

    feat(remove querty): remove query parameters - url looks nice

--- app/middleware/cookies.ts
+++ app/middleware/cookies.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 export default function cookiesMiddleware(req: Request, res: Response, next: NextFunction) {
   const cookieChoiceMade = req.cookies['cookieChoiceMade'];
-  const cookies = req.query.cookies;
-  res.locals.showBanner = cookieChoiceMade === undefined && cookies === undefined;
+  const cookies = req.body.cookies;
+  res.locals.showCookiesBanner = cookieChoiceMade === undefined && cookies === undefined;
 
   if(cookieChoiceMade === undefined && cookies) {
     res.cookie('cookieChoiceMade', cookies);
@@ -12,11 +12,21 @@ export default function cookiesMiddleware(req: Request, res: Response, next: Nex
   res.locals.allowCookies = cookieChoiceMade === 'accept' || cookies === 'accept';
 
   const hideCookieMessage = req.cookies['hideCookies'];
-  const hideCookies = req.query.hideCookies;
-  res.locals.showSecondBanner = !res.locals.showBanner && hideCookieMessage === undefined && hideCookies === undefined;
+  const hideCookies = req.body.hideCookies;
+  res.locals.showHideCookieMessageBanner = !res.locals.showCookiesBanner && 
+    hideCookieMessage === undefined && hideCookies === undefined;
   if(hideCookieMessage === undefined && hideCookies) {
     res.cookie('hideCookies', hideCookies);
   }
-  
-  next();
+
+  if(cookies === undefined && hideCookies === undefined) {
+    next();
+    return;
+  }
+
+  if(req.method === 'POST') {
+    res.redirect(req.originalUrl);
+  } else {
+    next();
+  }
 }
